13f564bddb9eafcc938e91ac0ae640d7e37ea2c5
Mallika Razdan <mallikarazdan@yahoo.co.in>
2016-07-26 09:35:14 -0400

    Added person.cpp

--- bullpen_sketch/person.cpp
+++ bullpen_sketch/person.cpp
@@ -1,7 +1,18 @@
 #include "person.hpp"
 
 bool Person::getPresent() {
-
   return present;
 }
+
+
+void Person::togglePresent(){
+  this->present = !present;
+}
+
+void Person::setPresent(bool present){
+  this->present = present;
+
+}
+
+
 
